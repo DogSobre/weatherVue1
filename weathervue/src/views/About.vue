b7e18c18cd81bf5bce7810611fef9a2fd4250c43
DogSobre <juanfernandoherrada13@gmail.com>
2020-09-30 17:00:00 +0200

    weather vue 1

--- weathervue/src/views/About.vue
+++ weathervue/src/views/About.vue
@@ -1,29 +1,37 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <b-col>{{ About }}</b-col>
-    <p>{{ meteoData }}</p>
-  </div>
+<div class="about">
+  <h1>This is an about page</h1>
+  <input @keypress.enter="setCity" v-model="tempcity">
+  <p> {{ getWeatherCity }} </p>
+</div>
 </template>
 
 <script>
 import axios from 'axios';
 
 export default {
+  data() {
+    return {
+      city: '',
+      tempcity: '',
+    };
+  },
   name: 'About',
   methods: {
     hideDP() {
-      console.log('toto');
+      console.log('hideDP method');
+    },
+    setCity() {
+      this.city = this.tempcity;
     },
   },
   asyncComputed: {
-    meteoData: {
+    getWeatherCity: {
       get() {
-        return axios.get('https://api.openweathermap.org/data/2.5/weather?q=Amiens&appid=d77e7612b116338a932892eae146d11c')
-          .then((response) => response.data);
+        return axios.get('https://api.openweathermap.org/data/2.5/weather', { params: { q: this.city, appid: 'd77e7612b116338a932892eae146d11c' } }).then((response) => response.data);
       },
       default() {
-        return 'Loading…';
+        return 'Loading';
       },
     },
   },
