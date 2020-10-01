2b6c62af4c4bf2e4bd487fe95fdfb2dff834f8ff
Unknown <unknown@example.com>
2020-10-01 21:19:56 +0200

    home CSS

--- weathervue/src/App.vue
+++ weathervue/src/App.vue
@@ -3,7 +3,6 @@
     <div id="nav">
       <router-link to="/about">Meteo</router-link> |
       <router-link to="/">Home</router-link>
-
     </div>
     <router-view/>
   </div>
--- weathervue/src/components/HelloWorld.vue
+++ weathervue/src/components/HelloWorld.vue
@@ -1,34 +1,11 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <section class="hero">
+      <div class="content">
+        <h2>welcom to the weather website by coding factory</h2>
+      </div>
+      <div class="waves"></div>
+    </section>
   </div>
 </template>
 
@@ -36,25 +13,131 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String,
   },
 };
 </script>
+function disableScroll(e){
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+if (e.keyCode) {
+/^(32|33|34|35|36|38|40)$/.test(e.keyCode) && e.preventDefault();
+}else {
+e.preventDefault();
 }
-ul {
-  list-style-type: none;
+
+}
+
+addEventListener("mousewheel", disableScroll, false);
+addEventListener("DOMMouseScroll", disableScroll, false);
+addEventListener("keydown", disableScroll, false);
+<style scoped>
+html{
+  overflow: scroll;
+  background-image: url("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.fr%2Fpin%2F571112796480678383%2F&psig=AOvVaw0ZNVjsuSQ00y-NaAYnrwNV&ust=1601666174290000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIjp7cyNlOwCFQAAAAAdAAAAABAO");
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #504c2c;
+}
+body {
+}
+
+#nav a.router-link-exact-active {
+  color: #ffd400;
+}
+
+:root {
+  --color: #0099ff;
+}
+
+body {
+  font-size: 16px;
+  font-family: 'Montserrat', sans-sherif,serif;
+  margin: 0;
   padding: 0;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.content {
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 0 20px;
 }
-a {
-  color: #42b983;
+
+.hero {
+  position: relative;
+  background: #333333;
+  color: white;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  overflow: hidden;
 }
+
+.hero h2 {
+  position: relative;
+  z-index: 1;
+  font-size: 4.5rem;
+  margin: 0 0 10px;
+  line-height: 1;
+  color: rgb(225, 225, 0);
+}
+
+.hero p {
+  position: relative;
+  z-index: 1;
+  font-size: 1.1rem;
+  color: rgba(255, 255, 255, 0.5);
+  line-height: 1.4;
+}
+
+/* ========================= */
+
+.waves {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 200px;
+  background-color: #ae5aff;
+  box-shadow: inset 0 0 50px #0097ff;
+  transition: 500ms;
+}
+
+.waves::before,
+.waves::after {
+  content: '';
+  position: absolute;
+  width: 300vw;
+  height: 300vw;
+  top: -65vw;
+  left: 50%;
+  transform: translate(-50%, -75%);
+}
+
+.waves::before {
+  border-radius: 44%;
+  background: #8743ff;
+  animation: waves 8s linear infinite;
+}
+
+.waves::after {
+  border-radius: 44%;
+  background: #9f76ff;
+  animation: waves 15s linear infinite;
+}
+
+@keyframes waves {
+  0% {
+    transform: translate(-50%, -75%) rotate(0deg);
+  }
+
+  100% {
+    transform: translate(-50%, -75%) rotate(360deg);
+  }
+}
+
 </style>
